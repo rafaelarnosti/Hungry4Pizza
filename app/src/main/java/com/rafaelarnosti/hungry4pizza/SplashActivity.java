f41123c5001288df4a5fc5bb5e0f9d9dd82e8e3f
Rafael Luiz Mazzini Arnosti <rlm.arnosti@gmail.com>
2017-05-29 22:20:50 -0300

    Commit Initial

--- app/src/main/java/com/rafaelarnosti/hungry4pizza/SplashActivity.java
+++ app/src/main/java/com/rafaelarnosti/hungry4pizza/SplashActivity.java
@@ -3,14 +3,11 @@ package com.rafaelarnosti.hungry4pizza;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-    public void logar(){
-        
+        setContentView(R.layout.activity_splash);
     }
 }
